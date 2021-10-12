7bf23afa526456a557fe45d2a109bbcc9ef22ffd
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2021-10-12 07:32:06 +0300

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store'
 import * as fromArticle from './redux/reducer'
+import * as fromAction from './redux/actions/articles'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,9 +12,9 @@ export class AppComponent {
   title = 'app';
 
   constructor(private store: Store<fromArticle.State>){
+    this.store.dispatch(new fromAction.AddArticleAction({id:1,link:'http:angular.io',title:'Angular',points:0}))
     this.store.select(fromArticle.getArticles).subscribe(v => {
       console.log(v)
     })
-  }
-}
+  }}
 
